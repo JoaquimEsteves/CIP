ca1537865972e9595118b46d11f6455e2e3d5462
Francisco Martins <franciscoprelhaz.m@gmail.com>
2017-04-17 17:27:21 +0100

    parsed with respective ips and ports of connected clients

--- TcpServer.py
+++ TcpServer.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import errno
 import thread
+import argparse
 from thread import start_new_thread
 
 PORT = 8888
@@ -19,24 +20,30 @@ except socket.error, v:
     sys.exit()
 
 print "Socket binding finished"
-tcp_socket.listen(16)
+tcp_socket.listen(16) #CHECK THIS OUT LATER THIS IS SUSPICIOUS AS FUUUUUUUUUUU
 
-def threaded_connection(connection):
+def threaded_connection(connection,IP,PORT):
     connection.send("Welcome, at the end of every command press ENTER")
     while (True):
         data = connection.recv(1024)
-        print data
-        if not data:
-            break
-        connection.sendall(data)
+        print "IP:" + str(IP) + " Port:" + str(PORT) + "Says:" +data
+        print "data stuff"
+        #if raw_input('A'):
+         #   break
+        #connection.sendall(data)
+        #print "senddata"
     connection.close()
 
 while(True):
-    connection, client_address = tcp_socket.accept()
-    print "Successfully connected with %s : %d fam" % (client_address[0], client_address[1])
-
-    #threading starts here
-    start_new_thread(threaded_connection, (connection,))
+    try:
+        connection, client_address = tcp_socket.accept()
+        print "Successfully connected with %s : %d fam" % (client_address[0], client_address[1])
+        #threading starts here
+        start_new_thread(threaded_connection, (connection,client_address[0], client_address[1],))
+    except KeyboardInterrupt, e:
+        # if CTRL+C is pressed, then go for last step
+        print "GETTING THE HELL OUT."
+        break
 
 tcp_socket.close()
 
