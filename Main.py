5d27ea248148bd55f989d4c840f4d2f8d764312f
Francisco Martins <franciscoprelhaz.m@gmail.com>
2017-04-27 19:38:01 +0100

    main

--- Main.py
+++ Main.py
@@ -6,7 +6,5 @@ from socket import *
 
 if __name__ == "main":
     log.info("Starting server")
-    tcp_socket = socket.socket(AF_INET, socket.SOCK_STREAM)
-    try:
-        client_address = tcp_socket.accept()
-        addresses = protocols.request(client_address[0], client_address[1]))
+    protocols.run()
+    for i in
