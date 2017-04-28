8ae5d41ecf90aa508eb243858c9d2749c65fcdd9
Francisco Martins <franciscoprelhaz.m@gmail.com>
2017-04-28 16:23:21 +0100

    comms

--- Main.py
+++ Main.py
@@ -4,6 +4,12 @@ from utils import Logger
 log = Logger(debug=settings.DEBUG)
 from socket import *
 
+
+
+#create 6 sockets, main handles each
+#main handles the procedure (1st roleer,qc etc...)
+#main states host and port
+#how do we call each station
 if __name__ == "main":
     log.info("Starting server")
     protocols.run()
